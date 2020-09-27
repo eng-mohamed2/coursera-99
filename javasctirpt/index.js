83c7a843376548d03903731bf602f10e6c2488b0
Maxamedxuseendaahir <70236862+Maxamedxuseendaahir@users.noreply.github.com>
2020-09-27 07:01:17 +0300

    somethng new

---
+++ javasctirpt/index.js
@@ -0,0 +1,8 @@
+function openslidemenu(){
+    document.getElementById('side-menu').style.width= '250px';
+    document.getElementById('main-part').style.marginLeft= '250px';
+}
+function closesidemenu(){
+    document.getElementById('side-menu').style.width= '0px';
+    document.getElementById('main-part').style.marginLeft= '0px';
+}
